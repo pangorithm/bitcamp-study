f16d9a0e330ed258c3293d5d38d3f9ce18e0f596
pangorithm <phs559800@gmail.com>
2023-05-30 17:02:57 +0900

    오후 수업

--- myapp/app/src/main/java/bitcamp/myapp/App.java
+++ myapp/app/src/main/java/bitcamp/myapp/App.java
@@ -14,40 +14,48 @@ public class App {
     // Scanner scanner = new java.util.Scanner(keyboard);
     Scanner scanner = new java.util.Scanner(System.in);
 
-    System.out.print("번호? ");
-    int no = scanner.nextInt();
+    int count = 3;
 
-    System.out.print("이름? ");
-    String name = scanner.next();
+    int[] no = new int[count];
+    String[] name = new String[count];
+    int[] age = new int[count];
+    boolean[] working = new boolean[count];
+    char[] gender = new char[count];
+    float[] leftEye = new float[count];
+    float[] rightEye = new float[count];
 
-    System.out.print("나이? ");
-    int age = scanner.nextInt();
+    for (int i = 0; i < count; i++) {
+      System.out.print("번호? ");
+      no[i] = scanner.nextInt();
 
-    System.out.print("재직중(true/false)? ");
-    boolean working = scanner.nextBoolean();
+      System.out.print("이름? ");
+      name[i] = scanner.next();
 
-    System.out.print("성별? ");
-    String str = scanner.next();
-    char gender = str.charAt(0);
+      System.out.print("나이? ");
+      age[i] = scanner.nextInt();
 
-    System.out.print("시력(왼쪽, 오른쪽)? ");
-    float leftEye = scanner.nextFloat();
-    float rightEye = scanner.nextFloat();
+      System.out.print("재직중(true/false)? ");
+      working[i] = scanner.nextBoolean();
+
+      System.out.print("성별? ");
+      String str = scanner.next();
+      gender[i] = str.charAt(0);
+
+      System.out.print("시력(왼쪽, 오른쪽)? ");
+      leftEye[i] = scanner.nextFloat();
+      rightEye[i] = scanner.nextFloat();
+    }
 
     System.out.println("----------------------------------------------");
+
+    for (int i = 0; i < count; i++) {
+      System.out.printf("번호: %d\n", no[i]);
+      System.out.printf("이름: %s\n", name[i]);
+      System.out.printf("나이: %d\n", age[i]);
+      System.out.printf("재직자: %b\n", working[i]);
+      System.out.printf("성별(남자(M), 여자(W)): %c\n", gender[i]);
+      System.out.printf("좌우시력: %.1f,%.1f\n", leftEye[i], rightEye[i]);
+    }
     scanner.close();
-
-    System.out.printf("번호: %d\n", no);
-
-    System.out.printf("이름: %s\n", name);
-
-    System.out.printf("나이: %d\n", age);
-
-    System.out.printf("재직자: %b\n", working);
-
-    System.out.printf("성별(남자(M), 여자(W)): %c\n", gender);
-
-    System.out.printf("좌우시력: %.1f,%.1f\n", leftEye, rightEye);
-
   }
 }
