0e0e19d627fb4fd0346149ed8e7190c82bdaeed8
pangorithm <phs559800@gmail.com>
2023-06-01 15:29:40 +0900

    오후 수업

--- myapp/app/src/main/java/bitcamp/myapp/App.java
+++ myapp/app/src/main/java/bitcamp/myapp/App.java
@@ -6,10 +6,7 @@ import java.util.Scanner;
 
 public class App {
   public static void main(String[] args) {
-    System.out.println("나의 목록 관리 시스템");
-    System.out.println("--------------------------------------------");
 
-    // 키보드 스캐너 준비
     // InputStream keyboard = System.in;
     // Scanner scanner = new java.util.Scanner(keyboard);
     Scanner scanner = new java.util.Scanner(System.in);
@@ -24,66 +21,82 @@ public class App {
     String[] password = new String[MAX_SIZE];
     char[] gender = new char[MAX_SIZE];
 
+    printTitle();
+
     for (int i = 0; i < MAX_SIZE; i++) {
-      System.out.print("이름? ");
-      name[i] = scanner.next();
 
-      System.out.print("email? ");
-      email[i] = scanner.next();
-
-      System.out.print("비밀번호? ");
-      password[i] = scanner.next();
-
-      loop: while (true) {
-        System.out.println("성별: ");
-        System.out.println("  1. 남자");
-        System.out.println("  2. 여자");
-        System.out.print(">");
-        String menuNo = scanner.next();
-
-        // if (menuNo.equals("1")) {
-        // gender[i] = 'M';
-        // break;
-        // } else if (menuNo.equals("2")) {
-        // gender[i] = 'W';
-        // break;
-        // } else {
-        // System.out.println("무효한 번호입니다.");
-        // }
-
-        switch (menuNo) {
-          case "1":
-            gender[i] = 'M';
-            // break; // 반복문을 나가는 것이 아니라 switch문을 나간다
-            break loop; // loop라는 라벨이 붙은 반복문을 나가라!
-          case "2":
-            gender[i] = 'W';
-            break loop;
-          default:
-            System.out.println("무효한 번호입니다.");
-        }
-
-      }
-
-      no[i] = userId++;
+      inputMember(scanner, i, name, email, password, gender, no, userId++);
 
       length++;
 
-      System.out.print("계속 하시겠습니까?(Y/n) ");
-      scanner.nextLine(); // 이전에 next()를 실행한 후 남아 있는 줄바꿈 코드를 제거한다.
-      String response = scanner.nextLine();
-      if (!response.equals("") && !response.equalsIgnoreCase("Y") && !response.equalsIgnoreCase("yes")) {
+      if (!promptContinue(scanner)) {
         break;
       }
     }
 
-    // 회원정보 등록
-    System.out.println("-------------------------------------");
+    printMembers(length, no, name, email, gender);
+
+    scanner.close();
+  }
+
+  static void printTitle() {
+    System.out.println("나의 목록 관리 시스템");
+    System.out.println("--------------------------------------------");
+  }
+
+  static void inputMember(Scanner scanner, int i, String[] name, String[] email, String[] password, char[] gender,
+      int[] no, int userId) {
+    System.out.print("이름? ");
+    name[i] = scanner.next();
+
+    System.out.print("email? ");
+    email[i] = scanner.next();
+
+    System.out.print("비밀번호? ");
+    password[i] = scanner.next();
+
+    loop: while (true) {
+      System.out.println("성별: ");
+      System.out.println("  1. 남자");
+      System.out.println("  2. 여자");
+      System.out.print(">");
+      String menuNo = scanner.next();
+      scanner.nextLine(); // 입력 값(token)을 읽고 난 후에 남아 있는 줄바꿈 코드를 제거한다.
+
+      switch (menuNo) {
+        case "1":
+          gender[i] = 'M';
+          // break; // 반복문을 나가는 것이 아니라 switch문을 나간다
+          break loop; // loop라는 라벨이 붙은 반복문을 나가라!
+        case "2":
+          gender[i] = 'W';
+          break loop;
+        default:
+          System.out.println("무효한 번호입니다.");
+      }
+
+    }
+
+    no[i] = userId;
+
+  }
+
+  static boolean promptContinue(Scanner scanner) {
+    System.out.print("계속 하시겠습니까?(Y/n) ");
+    String response = scanner.nextLine();
+    if (!response.equals("") && !response.equalsIgnoreCase("Y") && !response.equalsIgnoreCase("yes")) {
+      return false;
+    }
+    return true;
+  }
+
+  static void printMembers(int length, int[] no, String[] name, String[] email, char[] gender) {
+    // 회원정보 출력
+    System.out.println("--------------------------------------------");
     System.out.println("번호, 이름, 이메일, 성별");
-    System.out.println("-------------------------------------");
+    System.out.println("--------------------------------------------");
     for (int i = 0; i < length; i++) {
       System.out.printf("%d, %s, %s, %c\n", no[i], name[i], email[i], gender[i]);
     }
-    scanner.close();
   }
 }
