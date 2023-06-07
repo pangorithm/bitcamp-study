c40ab578a924ce6ed45a4fdb18f3b1f73544a8cc
pangorithm <phs559800@gmail.com>
2023-06-07 18:56:17 +0900

    강사님 파일과 코드 같게 수정

--- myapp/app/src/main/java/bitcamp/myapp/App.java
+++ myapp/app/src/main/java/bitcamp/myapp/App.java
@@ -1,12 +1,10 @@
 package bitcamp.myapp;
 
-import bitcamp.myapp.handler.MenberHandler;
+import bitcamp.myapp.handler.MemberHandler;
 import bitcamp.util.Prompt;
 
-// 코드 본문에서 사용할 클래스가 어떤 패키지를 클래스인지 지정한다.
-
 public class App {
-
+  
   public static void main(String[] args) {
 
     printTitle();
@@ -14,43 +12,27 @@ public class App {
     printMenu();
 
     while (true) {
-      String meneNo = Prompt.inputString("메인> ");
-      if (meneNo.equals("6")) {
+      String menuNo = Prompt.inputString("메인> ");
+      if (menuNo.equals("6")) {
         break;
-      } else if (meneNo.equals("menu")) {
+      } else if (menuNo.equals("menu")) {
         printMenu();
-      } else if (meneNo.equals("1")) {
-        MenberHandler.inputMember();
-      } else if (meneNo.equals("2")) {
-        MenberHandler.printMembers();
-      } else if (meneNo.equals("3")) {
-        MenberHandler.viewMember();
-      } else if (meneNo.equals("4")) {
-        MenberHandler.updateMember();
-      } else if (meneNo.equals("5")) {
-        MenberHandler.deleteMember();
+      } else if (menuNo.equals("1")) {
+        MemberHandler.inputMember();
+      } else if (menuNo.equals("2")) {
+        MemberHandler.printMembers();
+      } else if (menuNo.equals("3")) {
+        MemberHandler.viewMember();
+      } else if (menuNo.equals("4")) {
+        MemberHandler.updateMember();
+      } else if (menuNo.equals("5")) {
+        MemberHandler.deleteMember();
       } else {
-        System.out.println(meneNo);
+        System.out.println(menuNo);
       }
-
     }
 
     Prompt.close();
-  }
-
-  static void printTitle() {
-    System.out.println("나의 목록 관리 시스템");
-    System.out.println("------------------------------------");
-
-  }
-
-  static boolean promptContinue() {
-    String response = Prompt.inputString("계속 하시겠습니까?(Y/n) ");
-    if (!response.equals("") && !response.equalsIgnoreCase("Y") && !response.equalsIgnoreCase("yes")) {
-      return false;
-    } else {
-      return true;
-    }
   }
 
   static void printMenu() {
@@ -62,4 +44,16 @@ public class App {
     System.out.println("6. 종료");
   }
 
+  static void printTitle() {
+    System.out.println("나의 목록 관리 시스템");
+    System.out.println("----------------------------------");
+  }
+
+  static boolean promptContinue() {
+    String response = Prompt.inputString("계속 하시겠습니까?(Y/n) ");
+    if (!response.equals("") && !response.equalsIgnoreCase("Y")) {
+      return false;
+    }
+    return true;
+  }
 }
--- myapp/app/src/main/java/bitcamp/myapp/Test.java
+++ myapp/app/src/main/java/bitcamp/myapp/Test.java
@@ -1,15 +1,14 @@
 package bitcamp.myapp;
 
-// 소스 코드에서 Calculator 클래스는 bitcamp.util 패키지에 소속된 클래스를 갖는다.
+// 소스 코드에서 Calculator 클래스는 bitcamp.util 패키지에 소속된 클래스를 가리킨다.
 import bitcamp.util.Calculator;
 
 public class Test {
-
+  
   public static void main(String[] args) {
     // 2 * 3 + 7 - 2 / 2 = ?
     // => 연산자 우선 순위를 고려하지 않고 앞에서부터 뒤로 순차적으로 계산한다.
 
-    // int result; // 로컬 변수는 자동으로 초기화 되지 않는다.
     Calculator.init(2);
     Calculator.multiple(3);
     Calculator.plus(7);
@@ -18,4 +17,5 @@ public class Test {
     System.out.println(Calculator.result);
   }
 
+  
 }
