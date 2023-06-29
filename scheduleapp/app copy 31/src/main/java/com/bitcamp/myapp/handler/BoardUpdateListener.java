722d6f7a249a0bda3bd72491ef62f70bea9f0c2c
pangorithm <phs559800@gmail.com>
2023-06-29 21:07:58 +0900

    스캐줄앱 31단계 카피 & 백업

---
+++ scheduleapp/app copy 31/src/main/java/com/bitcamp/myapp/handler/BoardUpdateListener.java
@@ -0,0 +1,30 @@
+package com.bitcamp.myapp.handler;
+
+import java.util.List;
+import com.bitcamp.myapp.vo.Board;
+import com.bitcamp.util.BreadcrumbPrompt;
+
+public class BoardUpdateListener extends AbstractBoardListener {
+
+  public BoardUpdateListener(List<Board> list) {
+    super(list);
+  }
+
+  @Override
+  public void service(BreadcrumbPrompt prompt) {
+    Board board = this.list.get(findBy(prompt.inputInt("번호?")));
+    if (board == null) {
+      System.out.println("해당 번호의 게시글이 존재하지 않습니다.");
+    } else {
+      if (board.getPassword().equals(prompt.inputString("암호?"))) {
+        board.setTitle(prompt.inputString("제목? (%s)\n", board.getTitle()));
+        board.setContent(prompt.inputString("내용? (%s)\n", board.getContent()));
+        board.setCreatedDate(System.currentTimeMillis());
+      } else {
+        System.out.println("암호가 올바르지 않습니다.");
+      }
+    }
+  }
+
+
+}
