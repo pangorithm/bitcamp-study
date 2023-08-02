567e3176bede920129f08f787d7e883348a17f42
pangorithm <phs559800@gmail.com>
2023-08-02 12:34:35 +0900

    오전 수업

--- myapp/app-server/src/main/java/bitcamp/myapp/handler/BoardListServlet.java
+++ myapp/app-server/src/main/java/bitcamp/myapp/handler/BoardListServlet.java
@@ -33,13 +33,16 @@ public class BoardListServlet implements Servlet {
     out.println("</head>");
     out.println("<body>");
     out.println("<h1>게시글 목록 </h1>");
+    out.println("<div style='margin:5px;'>");
+    out.println("<a href='/board/form.html'>새 글</a>");
+    out.println("</div>");
     out.println("<table border='1'>");
     out.println("<thead>");
     out.println("   <tr><th>번호</th> <th>제목</th> <th>작성자</th> <th>조회수</th> <th>등록일</th></tr>");
     out.println("</thead>");
 
     out.println("<tbody>");
-    List<Board> list = boardDao.findAll(/* Integer.parseInt(request.param("category")) */ 1);
+    List<Board> list = boardDao.findAll(Integer.parseInt(request.getParameter("category")));
     for (Board board : list) {
       out.printf("<tr><td>%d</td> <td>%s</td> <td>%s</td> <td>%d</td> <td>%s</td></tr>\n",
           board.getNo(), board.getTitle(),
