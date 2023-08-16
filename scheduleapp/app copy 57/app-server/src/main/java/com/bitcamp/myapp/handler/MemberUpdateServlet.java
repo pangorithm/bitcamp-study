94b1a2d07ffb0fbaa2263f7739cb99f4a7e500f0
pangorithm <phs559800@gmail.com>
2023-08-16 17:45:33 +0900

    스캐줄앱 57단계 적용

---
+++ scheduleapp/app copy 57/app-server/src/main/java/com/bitcamp/myapp/handler/MemberUpdateServlet.java
@@ -0,0 +1,74 @@
+package com.bitcamp.myapp.handler;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import com.bitcamp.myapp.vo.Member;
+
+@WebServlet("/member/update")
+public class MemberUpdateServlet extends HttpServlet {
+
+  private static final long serialVersionUID = 1L;
+
+  @Override
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+
+    response.setContentType("text/html;charset=UTF-8");
+    PrintWriter out = response.getWriter();
+
+    Member loginUser = (Member) request.getSession().getAttribute("loginUser");
+    int memberNo = Integer.parseInt(request.getParameter("no"));
+    String newPassword = request.getParameter("password");
+
+    Member m = InitServlet.memberDao.findBy(memberNo);
+    if (m == null) {
+      System.out.println("해당 번호의 회원이 없습니다!");
+      return;
+    }
+
+    m.setName(request.getParameter("username").replaceAll("<script", "<scr!pt"));
+    m.setEmail(request.getParameter("email").replaceAll("<script", "<scr!pt"));
+    m.setTel(request.getParameter("tel").replaceAll("<script", "<scr!pt"));
+    m.setPassword(newPassword);
+    m.setGender(request.getParameter("gender").charAt(0));
+
+    out.println("<!DOCTYPE html>");
+    out.println("<html>");
+    out.println("<head>");
+    out.println("<meta charset='UTF-8'>");
+    out.println("<meta http-equiv='refresh' content='1;url=/member/list'>");
+    out.println("<title>회원</title>");
+    out.println("</head>");
+    out.println("<body>");
+    out.println("<h1>회원 변경</h1>");
+
+    try {
+      if (newPassword == null || newPassword.length() == 0) {
+        out.println("변경할 비밀번호를 입력해주세요.");
+      } else {
+        if (!m.equals(loginUser)) {
+          out.println("로그인한 계정만 수정 가능합니다.");
+        } else if (InitServlet.memberDao.update(m) == 0) {
+          out.println("해당 회원이 없습니다나.");
+        } else {
+          InitServlet.sqlSessionFactory.openSession(false).commit();
+          out.println("변경했습니다!");
+        }
+      }
+    } catch (Exception e) {
+      InitServlet.sqlSessionFactory.openSession(false).rollback();
+      out.println("회원 변경 실패입니다!");
+      e.printStackTrace();
+    }
+
+    out.println("</body>");
+    out.println("</html>");
+  }
+
+
+}
