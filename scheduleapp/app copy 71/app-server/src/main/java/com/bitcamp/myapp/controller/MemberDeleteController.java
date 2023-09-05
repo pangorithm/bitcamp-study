e7e4a885b305040b5bb193a8c36d6cba3107840d
pangorithm <phs559800@gmail.com>
2023-09-05 19:12:55 +0900

    개인 프로젝트 scheduleapp : [71. 트랜잭션 제어 코드를 캡슐화하기: Spring의 TransactionTemplate 모방 구현]

---
+++ scheduleapp/app copy 71/app-server/src/main/java/com/bitcamp/myapp/controller/MemberDeleteController.java
@@ -0,0 +1,40 @@
+package com.bitcamp.myapp.controller;
+
+import com.bitcamp.myapp.service.MemberService;
+import com.bitcamp.myapp.vo.Member;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller("/member/delete")
+public class MemberDeleteController implements PageController {
+
+  @Autowired
+  MemberService memberService;
+
+  @Override
+  public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+    Member loginUser = (Member) request.getSession().getAttribute("loginUser");
+    if (loginUser == null) {
+      return "redirect:../auth/login";
+    }
+
+    Member member = memberService.get(Integer.parseInt(request.getParameter("no")));
+    if (member == null || member.getNo() != loginUser.getNo()) {
+      throw new Exception("해당 번호의 멤버가 없거나 삭제 권한이 없습니다.");
+    }
+
+    try {
+      memberService.delete(member.getNo());
+      return "redirect:list";
+    } catch (Exception e) {
+      request.setAttribute("message", e.getMessage());
+      request.setAttribute("refresh", "2;url=list");
+      throw e;
+
+    }
+  }
+
+}
---
+++ scheduleapp/app copy 71/app-server/src/main/webapp/WEB-INF/jsp/index.jsp
@@ -0,0 +1,35 @@
+<%@ page
+    language="java"
+    pageEncoding="utf-8"
+    contentType="text/html;charset=utf-8"%>
+<%@ page import="com.bitcamp.myapp.vo.Member"%>
+
+    <!DOCTYPE html>
+    <html>
+    <head>
+    <meta charset='UTF-8'>
+    <title>비트캠프</title>
+    </head>
+    <body>
+
+<jsp:include page="header.jsp"/>
+
+    <h1>ScheduleApp(JSP)</h1>
+    <p>개인 프로젝트</p>
+
+<jsp:include page="footer.jsp"/>
+
+    </body>
+    </html>
+
+
+
+
+
+
+
+
+
+
+
+
