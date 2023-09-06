5423ed395e2626b1278a67f6b62ad016010af202
pangorithm <phs559800@gmail.com>
2023-09-06 15:41:12 +0900

    수업 myapp : Spring WebMVC 프레임워크 도입하기

--- myapp/app-server/src/main/java/bitcamp/myapp/config/MyWebApplicationInitializer.java
+++ myapp/app-server/src/main/java/bitcamp/myapp/config/MyWebApplicationInitializer.java
@@ -1,12 +1,8 @@
 package bitcamp.myapp.config;
 
-import javax.servlet.MultipartConfigElement;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRegistration;
 import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.servlet.DispatcherServlet;
 
 public class MyWebApplicationInitializer implements WebApplicationInitializer {
 
@@ -14,16 +10,16 @@ public class MyWebApplicationInitializer implements WebApplicationInitializer {
   public void onStartup(ServletContext sc) throws ServletException {
     System.out.println("MyWebApplicationInitializer.onStartup() 호출됨!");
 
-    // DispatcherServlet이 사용할 IoC 컨테이너를 준비한다.
-    AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
-    context.register(AppConfig.class);
-
-    // DispatcherServlet을 생성하여 서블릿 컨테이너에 등록한다.
-    DispatcherServlet servlet = new DispatcherServlet(context);
-    ServletRegistration.Dynamic registration = sc.addServlet("app", servlet);
-    registration.setLoadOnStartup(1);
-    registration.addMapping("/app/*");
-    registration.setMultipartConfig(
-        new MultipartConfigElement("temp", 10000000, 15000000, 1000000));
+//    // DispatcherServlet이 사용할 IoC 컨테이너를 준비한다.
+//    AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+//    context.register(AppConfig.class);
+//
+//    // DispatcherServlet을 생성하여 서블릿 컨테이너에 등록한다.
+//    DispatcherServlet servlet = new DispatcherServlet(context);
+//    ServletRegistration.Dynamic registration = sc.addServlet("app", servlet);
+//    registration.setLoadOnStartup(1);
+//    registration.addMapping("/app/*");
+//    registration.setMultipartConfig(
+//        new MultipartConfigElement("temp", 10000000, 15000000, 1000000));
   }
 }
